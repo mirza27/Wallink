562eda7d9c7508a6f8e2d45be5f5160686744978
ApriliaCristy <ianaleale123@gmail.com>
2024-04-01 22:12:22 +0700

    main keri

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:wallink_v1/page/category_page.dart';
-
+//import 'package:wallink_v1/page/category_page.dart';
+import 'package:wallink_v1/page/intro2_page.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CategoryPage()
+    return MaterialApp(
+      title: 'WALINK',
+      home: IntroPage(),
     );
   }
 }
-

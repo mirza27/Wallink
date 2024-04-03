3f4daab1b4cb9a2e7c0844a855b2d521999d9393
mirza27 <ramadhanimirza10@gmail.com>
2024-04-03 20:30:06 +0700

    udapte intor

--- lib/page/intro1_page.dart
+++ lib/page/intro1_page.dart
@@ -1,9 +1,33 @@
-// intro1_page.dart
-
 import 'package:flutter/material.dart';
-import 'package:wallink_v1/page/intro2_page.dart'; // Import IntroPage2
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wallink_v1/page/category_page.dart';
+import 'package:wallink_v1/page/intro2_page.dart';
 
-class IntroPage1 extends StatelessWidget {
+class IntroPage1 extends StatefulWidget {
+  @override
+  _IntroPage1State createState() => _IntroPage1State();
+}
+
+class _IntroPage1State extends State<IntroPage1> {
+  @override
+  void initState() {
+    super.initState();
+    _checkOnboardingStatus();
+  }
+
+  void _checkOnboardingStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+    if (isFirstTime) {
+      await prefs.setBool('isFirstTime', false);
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => CategoryPage()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

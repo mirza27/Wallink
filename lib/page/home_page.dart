a8ac3ee5584389ab556b3e18b3a6eb0f6399975b
mirza27 <ramadhanimirza10@gmail.com>
2024-04-24 19:09:46 +0700

    search works

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wallink_v1/controller/link_controller.dart';
+import 'package:wallink_v1/models/link.dart';
 import 'package:wallink_v1/models/sub_category.dart';
 import 'package:wallink_v1/controller/sub_category_controller.dart';
 import 'package:wallink_v1/widgets/category_mini_card.dart';
+import 'package:wallink_v1/widgets/link_card.dart';
 import 'package:wallink_v1/widgets/sub_category_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +15,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> links = []; // menyimpan semua link secara statis
+  List<Map<String, dynamic>> _links =
+      []; // menyimpan link  dinamis (burubah saat searching)
   List<Map<String, dynamic>> _subCategories = [];
+  final TextEditingController _searchController = TextEditingController();
   int? _categoryId = null;
   bool _isSearching = false;
 
@@ -20,6 +27,24 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _loadData();
+  }
+
+  // logika search
+  Future<void> _search(String keywords) async {
+    List<Map<String, dynamic>> results = [];
+    if (keywords.isEmpty) {
+      results = _links;
+    } else {
+      results = links
+          .where((link) => link['link_name']
+              .toString()
+              .toLowerCase()
+              .contains(keywords.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _links = results;
+    });
   }
 
   // memilih category setelah dipanggil pada mini card
@@ -33,17 +58,26 @@ class _HomePageState extends State<HomePage> {
 
   // refresh dan load data
   Future<void> _loadData() async {
-    List<Map<String, dynamic>> subCategories =
-        await getSubCategoryByCategoryId(_categoryId); // parameter dari widget
+    List<Map<String, dynamic>> subCategories = await getSubCategoryByCategoryId(
+        _categoryId); // mengisi link berdasarkan sub
+    links = await getAllLink(); // Menyimpan semua data link statis
 
     setState(() {
       _subCategories = subCategories;
+      _links = links;
     });
   }
 
+  // FUNGSI SUBCATEGORY
   Future<void> _tes(int tes) async {}
 
   Future<void> _tes2(SubCategory tes) async {}
+
+  // FUNGSI LINK
+  // delete link
+  Future<void> _deleteLink(int linkId) async {}
+
+  Future<void> _editLink(Link link) async {}
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +85,7 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: const Color.fromRGBO(249, 249, 251, 1),
       appBar: AppBar(
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(91),
+          preferredSize: const Size.fromHeight(120),
           // Search field
           child: Container(
             decoration: const BoxDecoration(
@@ -74,44 +108,56 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.only(top: 10),
               child: Column(
                 children: [
-                  SearchAnchor(builder:
-                      (BuildContext context, SearchController controller) {
-                    return SearchBar(
-                      backgroundColor:
-                          MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                          if (states.contains(MaterialState.disabled)) {
-                            return const Color.fromRGBO(239, 240, 243, 1); // Warna latar belakang saat dalam keadaan disabled
-                          }
-                          return const Color(10); // Warna latar belakang default
-                        },
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: TextField(
+                      onChanged: (value) {
+                        // logika untuk mengubah colomn body
+                        if (value.isNotEmpty) {
+                          _isSearching = true;
+                        } else {
+                          _isSearching = false;
+                        }
+                        _search(value);
+                      },
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(Icons.search),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 15.0),
+                        filled: true,
+                        focusColor: Color.fromRGBO(5, 105, 220, 1),
+                        fillColor: Color.fromRGBO(239, 240, 243, 1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(239, 240, 243, 1),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(239, 240, 243, 1),
+                            width: 1.0,
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(239, 240, 243, 1),
+                            width: 1.0,
+                          ),
+                        ),
+                        hintText: 'Search Your Link',
+                        hintStyle: const TextStyle(
+                          fontSize: 16.0,
+                          color: Color.fromRGBO(139, 141, 152,
+                              1), // Ganti dengan warna yang diinginkan
+                        ),
+                        alignLabelWithHint: true,
                       ),
-                      hintText: "Search Your Link",
-                      controller: controller,
-                      padding: const MaterialStatePropertyAll<EdgeInsets>(
-                          EdgeInsets.symmetric(horizontal: 16.0)),
-                      onTap: () {
-                        controller.openView();
-                      },
-                      onChanged: (_) {
-                        controller.openView();
-                      },
-                    );
-                  }, suggestionsBuilder:
-                      (BuildContext context, SearchController controller) {
-                    return List<ListTile>.generate(5, (int index) {
-                      final String item = 'item $index';
-                      return ListTile(
-                        title: Text(item),
-                        onTap: () {
-                          setState(() {
-                            controller.closeView(item);
-                          });
-                        },
-                      );
-                    });
-                  }),
-
+                    ),
+                  ),
                   // mini card
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 20),
@@ -130,28 +176,50 @@ class _HomePageState extends State<HomePage> {
       ),
 
       // List subcategory tiles
-      body: Column(
-        children: [
-          Expanded(
-            child: SizedBox(
-              child: ListView.builder(
-                  // iterasi widget sub category card
-                  itemCount: _subCategories.length,
-                  itemBuilder: (context, index) {
-                    final SubCategory subCategory =
-                        SubCategory.fromMap(_subCategories[index]);
+      body: !_isSearching //
+          ? Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      // iterasi widget sub category card
 
-                    return SubCategoryCard(
-                      // parameter kelas subcategory
-                      subCategory: subCategory,
-                      onDelete: _tes,
-                      onUpdate: _tes2,
-                    );
-                  }),
+                      itemCount: _subCategories.length,
+                      itemBuilder: (context, index) {
+                        final SubCategory subCategory =
+                            SubCategory.fromMap(_subCategories[index]);
+
+                        return SubCategoryCard(
+                          // parameter kelas subcategory
+                          subCategory: subCategory,
+                          onDelete: _tes,
+                          onUpdate: _tes2,
+                        );
+                      }),
+                ),
+              ],
+            )
+          // jika melakukan searching
+          : Column(
+              children: [
+                Expanded(
+                    child: SizedBox(
+                  child: ListView.builder(
+                    itemCount: _links.length,
+                    itemBuilder: ((context, index) {
+                      final Link link = Link.fromMap(_links[index]);
+
+                      return LinkCard(
+                          key: ValueKey(link.id),
+                          link: link,
+                          onDelete: _deleteLink,
+                          onUpdate: _editLink,
+                          onChanged: _loadData);
+                    }),
+                  ),
+                ))
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }

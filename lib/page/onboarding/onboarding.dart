7e754bb0628d2520567b308258e1000d82851656
unknown <fauziyahdewi21@gmail.com>
2024-06-06 10:05:51 +0700

    splashscreen, onboarding, logo

---
+++ lib/page/onboarding/onboarding.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:wallink_v1/page/onboarding/screen1.dart';
+import 'package:wallink_v1/page/onboarding/screen2.dart';
+import 'package:wallink_v1/page/onboarding/screen3.dart';
+import 'package:wallink_v1/page/onboarding/screen4.dart';
+import 'package:wallink_v1/route_page.dart';
+
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({super.key});
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  PageController pageController = PageController();
+  bool _isLoading = false;
+  int _currentIndex = 0;
+
+  void _home() {
+    setState(() {
+      _isLoading = true;
+    });
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => RoutePage(
+          selectedIndex: 0,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            controller: pageController,
+            children: const [Screen1(), Screen2(), Screen3(), Screen4()],
+          ),
+          if (_currentIndex != 3)
+            Align(
+              alignment: Alignment.topRight,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 15.0, top: 10.0),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RoutePage(
+                            selectedIndex: 0,
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Skip',
+                      style: TextStyle(
+                        fontFamily: 'sharp',
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          if (_currentIndex == 3)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _isLoading ? null : _home,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 15),
+                        backgroundColor: const Color.fromARGB(255, 5, 105, 220),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(26),
+                        ),
+                      ),
+                      child: _isLoading
+                          ? const SizedBox(
+                              height: 25,
+                              width: 25,
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 2.0,
+                              ),
+                            )
+                          : const Text(
+                              'Get Started',
+                              style: TextStyle(
+                                fontFamily: 'sharp',
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          Container(
+            alignment: const Alignment(0, 0.8),
+            child: SmoothPageIndicator(
+              controller: pageController,
+              count: 4,
+              effect: const ExpandingDotsEffect(
+                activeDotColor: Color.fromARGB(255, 5, 105, 220),
+                dotWidth: 10,
+                dotHeight: 10,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/page/onboarding/screen1.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var orientation = MediaQuery.of(context).orientation;
+    var isLandscape = orientation == Orientation.landscape;
+
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 5, 105, 220),
+              Color.fromARGB(0, 255, 255, 255),
+              Colors.transparent
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: isLandscape ? 16.0 : 40.0,
+              horizontal: 16.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                Image.asset(
+                  "assets/welcome-illustration.png",
+                  width: isLandscape
+                      ? MediaQuery.of(context).size.width * 0.5
+                      : MediaQuery.of(context).size.width * 0.9,
+                ),
+                SizedBox(height: isLandscape ? 10 : 20),
+                 Text(
+                    'Selamat Datang di Wallink!',
+                    style: GoogleFonts.urbanist(
+                      fontSize: isLandscape ? 18 : 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                Padding(
+                  padding: isLandscape
+                      ? const EdgeInsets.all(10)
+                      : const EdgeInsets.all(20),
+                  child: Text(
+                    'Mulai pengalaman baru dalam menyimpan link favorit Anda dengan lebih efisien dan terstruktur.',
+                    style: GoogleFonts.urbanist(
+                      fontSize: isLandscape ? 16 : 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: isLandscape ? 20 : 200),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
